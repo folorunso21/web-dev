307509d9f17b3af085fd0b733123442840a882ab
folorunso21 <folorunsoolamilekan2006@gmail.com>
2023-04-18 11:31:51 +0100

    first

---
+++ java.js
@@ -0,0 +1,9 @@
+// console.log ("My name is Olamilekan Emmanuel");
+// document.write("I am a boy")
+// window.alert("This page is not comming up")
+// prompt("lekan")
+var first =Number(document.getElementById("userInput").value)
+var second = Number(document.getElementById("useInput").value)
+var third = (document.getElementById("usInput"))
+console.log(first+second)
+third.value = first+second;
